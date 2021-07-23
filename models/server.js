64a07811e36b4f9d1609735aaaba965c5fecfd7a
Alex <alexgomeznear@gmail.com>
2021-07-22 23:54:12 -0500

    base del proyecto

---
+++ models/server.js
@@ -0,0 +1,44 @@
+//Servidor de express
+const express = require('express');
+const http =  require('http');
+const socketio = require('socket.io');
+const path  = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+   constructor(){
+      this.app  = express();;
+      this.port = process.env.PORT;
+
+      //Http server
+      this.server = http.createServer(this.app);
+      
+      //Config sockets
+      this.io = socketio(this.server);
+   }
+
+   middlewares(){
+      // Desplegar el directorio publico
+      this.app.use(express.static(path.resolve(__dirname , '../public')));
+   }
+
+   configSockets(){
+      new Sockets( this.io );
+   }
+
+   execute(){
+
+      //Init middlewares
+      this.middlewares();
+
+      //Init Sockets
+      this.configSockets();
+
+      //Init Server
+      this.server.listen(this.port, () =>{
+         console.log('Server port:',this.port);
+      } );
+   }
+}
+
+module.exports = Server;
